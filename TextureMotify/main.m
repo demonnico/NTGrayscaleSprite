decd1fdef2f8b3f7ce6b372f1f4c3368349c989e
Nicholas Tau <demon42111915@gmail.com>
2012-11-05 19:57:30 +0800

    Initial Commit

---
+++ TextureMotify/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TextureMotify
+//
+//  Created by Tau Nicholas on 11/5/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
